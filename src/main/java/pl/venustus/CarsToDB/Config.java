27e85dd89691c25a27954944fdb3713ad3a9106a
venustuspl <venustus.pl@gmail.com>
2020-03-05 21:14:00 +0100

    0.0.2

---
+++ src/main/java/pl/venustus/CarsToDB/Config.java
@@ -0,0 +1,36 @@
+package pl.venustus.CarsToDB;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class Config {
+
+    @EventListener(ApplicationReadyEvent.class)
+    public static void dbInit() {
+        String sql = "CREATE TABLE CAR "
+
+    }
+
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.url("jdbc:mysql://localhost:3306/task_crud?serverTimezone=Europe/Warsaw&SSL=False");
+        dataSourceBuilder.username("nazwa_uzytkownika");
+        dataSourceBuilder.password("twoje_haslo");
+        dataSourceBuilder.driverClassName("com.mysql.jdbc.Driver");
+        return dataSourceBuilder.build();
+    }
+
+    @Bean
+    public JdbcTemplate getJdbcTemplate() {
+        return new JdbcTemplate(getDataSource());
+    }
+
+}
